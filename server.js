0e21e6371b6adec3260fbfede5c618c68cccd124
NandaSatriaPutra <nsatria993@gmail.com>
2020-05-04 15:19:58 +0700

    panggil routes di server

--- server.js
+++ server.js
@@ -5,6 +5,9 @@ const app = express();
 //parse application/json
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
+//panggil routes
+var routes = require('./routes');
+routes(app);
  app.listen(3000,() => {
      console.log('Server Started on port 3000');
  }); 
