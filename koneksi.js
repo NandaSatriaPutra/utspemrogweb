a6f6acb609a5db9bf1a2a9bbdeda3f3e3f0f363a
NandaSatriaPutra <nsatria993@gmail.com>
2020-05-04 15:11:59 +0700

    koneksi.js

---
+++ koneksi.js
@@ -0,0 +1,15 @@
+var mysql = require('mysql');
+//koneksi database
+const conn = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'bengkel'
+});
+
+conn.connect((err)=>{
+    if(err) throw err;
+    console.log('Mysql Terkoneksi');
+});
+
+module.exports = conn;
